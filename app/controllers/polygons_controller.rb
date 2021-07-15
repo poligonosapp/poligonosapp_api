3777afa63d88586cf2dc9adf82b8383764ad30c7
Luis Mendes <machadoluismendes@gmail.com>
2021-07-15 18:32:56 -0300

    github octokit

--- app/controllers/polygons_controller.rb
+++ app/controllers/polygons_controller.rb
@@ -1,2 +1,11 @@
 class PolygonsController < ApplicationController
+    # Provide authentication credentials
+client = Octokit::Client.new(:access_token => 'repo-token')
+
+# You can still use the username/password syntax by replacing the password value with your PAT.
+# client = Octokit::Client.new(:login => 'defunkt', :password => 'personal_access_token')
+
+# Fetch the current user
+client.user
+
 end
